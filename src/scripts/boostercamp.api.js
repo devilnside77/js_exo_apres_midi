d4357bab7c2afaa9120ea1b54f9b680020b92db4
JOYE Arnaud <joye.arnaud@gmail.com>
2018-09-23 18:10:11 +0200

    revue de code api

--- src/scripts/boostercamp.api.js
+++ src/scripts/boostercamp.api.js
@@ -3,8 +3,8 @@ import Tool from '../scripts/tool';
 import xhr from '../scripts/XhrUtils';
 
 const bareApi = {
-    'getTools' : (entity = null) => xhr('GET', 'http://localhost:3000/tools', entity, entity),
-    'getSoatiens' : (entity = null) => xhr('GET', 'http://localhost:3000/soatiens', entity),
+    'getTools' : () => xhr('GET', 'http://localhost:3000/tools', null),
+    'getSoatiens' : () => xhr('GET', 'http://localhost:3000/soatiens', null),
     'postSoatiens' : (entity) => xhr('POST', 'http://localhost:3000/soatiens', JSON.stringify(entity))
 };
 
